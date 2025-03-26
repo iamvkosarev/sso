5002fcc7aeddfcb54d29758d85549043ff72ae20
Kosarev Vsevolod <iamvkosarev@gmail.com>
2025-03-26 20:11:28 +0800

    Fixed migration format

--- migrations/20250325105800_init.sql
+++ migrations/20250325105800_init.sql
@@ -1,14 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     email VARCHAR(255) UNIQUE NOT NULL,
-    pass_hash TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT now()
-    );
--- +goose StatementEnd
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  pass_hash TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT now()
+);
 
+-- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS users;
+
 -- +goose StatementEnd
